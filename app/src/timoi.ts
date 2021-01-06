1be5721ea16dee8670d2b5927f292dbe57d43f8c
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-06 03:58:03 +0100

    functional

--- app/src/timoi.ts
+++ app/src/timoi.ts
@@ -1,7 +1,16 @@
+import pretty from "pretty-ms"
 
+function now() {
+  return +new Date()
+}
 
 export function timoi() {
-  
+  const start = now()
+  const time = () => now() - start
+  const done = () => {console.log(pretty(time())); return done}
+  done.time = time
+
+  return done as typeof done & { time: () => number }
 }
 
 export default timoi
