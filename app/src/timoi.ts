51536cf2b3f0aab02e18804da7ad3d08b3319e66
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-06 04:08:53 +0100

    typies

--- app/src/timoi.ts
+++ app/src/timoi.ts
@@ -7,7 +7,7 @@ function now() {
 export function timoi() {
   const start = now()
   const time = () => now() - start
-  const done = () => {console.log(`Took ${str()}`); return done}
+  const done = () => {console.log(`Took ${str()}`); return done as typeof done}
   const str = () => pretty(time())
   done.time = time
   done.str = str
